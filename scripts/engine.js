97dbcffd1de15655eb77fa6024b3149b7c048231
allanvini <54002547+allanvini@users.noreply.github.com>
2020-01-13 18:14:25 -0300

    Add files via upload

--- scripts/engine.js
+++ scripts/engine.js
@@ -1,4 +1,105 @@
-var namesDataBase = "NetScape 1991 Carpenter Brut Miami Hollywood Windows Macintosh Cyber Razor 1984 Heat Summer Sony Silverhand Noir City Infiltrator Mega Dropzone Defender Neon L.A. Angel MadMax Stereo Roland 808 Buck Power Gloves Nintendo Turbo Nitro Thunder FM Cassette Night Stud Velvet Future Techno DX Transistor Cell Surrender Prophet Fairlight Jupiter Electro Venus Throttle Ferrarri Lamborghini Testarossa Cruise Atari Panther Viper Cobra Cycle Boulevard Metropolis Ninja Lipstick 2049 Violator Robotron Body Leotard Rouge BladeRunner Eyeshadow Filter Arpeggiator Lowpass Handheld 2077 VCR Betamax LP Burner Colecovision 2600 Omnicorp Madame Tech System Cyberdyne Bosch Trevor Tiffany Kylie Asteroid Polybius Odyssey Lazer Photon Tyrell Vinyl Arnold Magnum";
+var namesDataBase = ["NetScape 1991",
+    "Carpenter",
+    "Brut",
+    "Miami",
+    "Hollywood",
+    "Windows",
+    "Macintosh",
+    "Cyber",
+    "Razor",
+    "1984",
+    "Heat",
+    "Summer",
+    "Sony",
+    "Silverhand",
+    "Noir",
+    "City",
+    "Infiltrator",
+    "Mega",
+    "Dropzone",
+    "Defender",
+    "Neon",
+    "L.A.",
+    "Angel",
+    "MadMax",
+    "Stereo",
+    "Roland",
+    "808",
+    "Buck",
+    "Power",
+    "Gloves",
+    "Nintendo",
+    "Turbo",
+    "Nitro",
+    "Thunder",
+    "FM",
+    "Cassette",
+    "Night",
+    "Stud",
+    "Velvet",
+    "Future",
+    "Techno",
+    "DX",
+    "Transistor",
+    "Cell",
+    "Surrender",
+    "Prophet",
+    "Fairlight",
+    "Jupiter",
+    "Electro",
+    "Venus",
+    "Throttle",
+    "Ferrarri",
+    "Lamborghini",
+    "Testarossa",
+    "Cruise",
+    "Atari",
+    "Panther",
+    "Viper",
+    "Cobra",
+    "Cycle",
+    "Boulevard",
+    "Metropolis",
+    "Ninja",
+    "Lipstick",
+    "2049",
+    "Violator",
+    "Robotron",
+    "Body",
+    "Leotard",
+    "Rouge",
+    "BladeRunner",
+    "Eyeshadow",
+    "Filter",
+    "Arpeggiator",
+    "Lowpass",
+    "Handheld",
+    "2077",
+    "VCR",
+    "Betamax",
+    "LP",
+    "Burner",
+    "Colecovision",
+    "2600",
+    "Omnicorp",
+    "Madame",
+    "Tech",
+    "System",
+    "Cyberdyne",
+    "Bosch",
+    "Trevor",
+    "Tiffany",
+    "Kylie",
+    "Asteroid",
+    "Polybius",
+    "Odyssey",
+    "Lazer",
+    "Photon",
+    "Tyrell",
+    "Vinyl",
+    "Arnold",
+    "Magnum"
+];
 
 function start() {
     var div = document.getElementById('box');
@@ -21,15 +122,15 @@ function getRandomNumber(min, max) {
 }
 
 function generateName() {
-    var nameList = namesDataBase.split(' ');
-    var listLength = (nameList.length) - 1;
+    //var nameList = namesDataBase.split(' ');
+    var listLength = (namesDataBase.length) - 1;
     var nameLength = getRandomNumber(2, 4);
 
     var PowerName = [];
 
     for (var count = 0; count < nameLength; count++) {
         var wordIndex = getRandomNumber(0, listLength);
-        PowerName.push(nameList[wordIndex]);
+        PowerName.push(namesDataBase[wordIndex]);
     }
 
     PowerName = PowerName.join(' ');
